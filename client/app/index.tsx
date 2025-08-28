e4c2f89e9b3465ef167982c05d9f939ee86d039e
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2025-08-28 22:49:02 +0530

    chessboard component and play route

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-2xl font-bold">Hello NativeWind 👋</Text>
+    <View className="flex-1 items-center justify-center bg-black gap-10">
+      <Text className="text-3xl font-bold  text-white text-center">Click below to Play Chess</Text>
+      <Link href="/play" className=" text-white text-3xl bg-gray-700 w-1/2 text-center rounded-xl">
+        <TouchableOpacity>
+          Play
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
